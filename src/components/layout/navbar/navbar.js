bb21456964e09747aae1aa9dd83ff304d8d7b4ba
Developer Jahid <jhpabel@gmail.com>
2020-03-23 11:53:09 +0600

    Initial commit from gatsby: (https://github.com/developerjahid/my-gatsby.git)

---
+++ src/components/layout/navbar/navbar.js
@@ -0,0 +1,12 @@
+import React, { Fragment} from "react"
+import Nav1 from "./nav/nav1"
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <Nav1 />
+    </Fragment>
+  )
+}
+
+export default Navbar
